0e0c8e268ec86e7fd6dc458b8a6b08bea284ac9a
vdhopteprinceton <vdhopte@princeton.edu>
2021-05-14 12:35:21 -0400

    sounds

---
+++ src/audio/index.js
@@ -0,0 +1,4 @@
+export { default as Soundtrack } from './Desert.mp3';
+export { default as WolfHowl } from './howl.mp3';
+export { default as WolfGrowl } from './growl.mp3';
+export { default as Sheepbaa } from './baa.mp3';
